25bfa1e00f7397d2da151cf77123d4f88e60c24c
sbrick26 <swayambarik@gmail.com>
2020-09-24 00:56:36 -0500

    smol change

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,17 +2,15 @@ const itemsContainer = document.getElementById('items')
 import data from './data.js'
 
 for (let i=0; i<data.length; ++i) {
-    // create a new div element and give it a class name
+    
     let newDiv = document.createElement('div');
     newDiv.className = 'item'
 
-    // create an image element
     let img = document.createElement('img');
-    // this will change each time we go through the loop.
     img.src = data[i].image
     img.width = 300
     img.height = 300
-     // Add the image to the div
+
     newDiv.appendChild(img)
 
     let desc = document.createElement('P')
