1e38f4115916fc3b47647b473ee9a9cb4153bdae
sbrick26 <swayambarik@gmail.com>
2020-10-09 01:30:01 -0500

    added removeItem function

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -30,3 +30,75 @@ for (let i=0; i<data.length; ++i) {
 
     itemsContainer.appendChild(newDiv)
 }
+
+    const cart = []
+
+    
+    function addItem(name, price) {
+      for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+             return
+        }
+
+      }
+      
+      
+      const item = {name: name, price: price, qty: 1}
+      cart.push(item)
+    }
+
+    function showItems() {
+      
+
+      
+
+      console.log(`Total in cart: $${getTotal()}`)
+      
+      console.log(`You have ${getQty()} items in your cart`)
+
+      for (let i = 0; i < cart.length; i +=1) {
+        console.log(`${cart[i].name}`)
+      }
+    }
+
+    function getQty() {
+      let qty = 0
+      for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty
+      }
+
+      return qty
+    }
+
+    function getTotal () {
+      let total = 0
+      for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].qty * cart[i].price
+      }
+
+      return total.toFixed(2)
+    }
+
+
+
+    function removeItem(name, qty = 0) { 
+        for (let i = 0; i < cart.length; i += 1) {
+            if (cart[i].name === name) {
+                if (qty > 0) {
+                    cart[i].qty -= qty
+                }
+               
+                if (carti[i].qty < 1 || qty === 0) {
+                    cart.splice(i, 1)
+                }
+                
+                return 
+            }
+        }
+    }
+
+    addItem('Apple', 0.99)
+    removeItem('Apple')
+    
+    showItems()
